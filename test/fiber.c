0b28dfa7b4b6dcf1eb9a4e4da076883d100e1f41
uael <abel@tempow.com>
2018-04-30 09:51:51 +0200

    osi: added some test cases

--- test/fiber.c
+++ test/fiber.c
@@ -18,24 +18,53 @@
 
 #include <osi/fiber.h>
 #include <osi/sched.h>
-#include <stdio.h>
-#include <string.h>
-#include <strings.h>
 
-static void fiber_func(void *ctx)
+#include <assert.h>
+
+static int counter = 0;
+
+static void fiber_func_0(void *ctx)
 {
 	for (int i = 0; i < 10; ++i) {
-		printf("%d [%d/%d]\n", *(int *)ctx, i + 1, 10);
+		if (++counter > 10)
+			counter = 1;
+		assert(counter == *(int *)ctx);
 		osi_yield();
 	}
-	printf("%d done\n", *(int *)ctx);
 }
 
-int main(void) {
+static void fiber_func_1(void *ctx)
+{
+	if (++counter > 10)
+		counter = 1;
+	for (int i = 0; i < 10; ++i) {
+		assert(counter == *(int *)ctx);
+	}
+}
+
+int test_0(void)
+{
 	int names[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 	for (int i = 0; i < 10; i++)
-		osi_fib_create(fiber_func, names + i, 1024, 1);
+		osi_fib_create(fiber_func_0, names + i, 1024, 1);
 	osi_schedule();
+	return 0;
+}
+
+int test_1(void)
+{
+	int names[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	for (int i = 0; i < 10; i++)
+		osi_fib_create(fiber_func_1, names + i, 1024, 1);
+	osi_schedule();
+	return 0;
+}
+
+int main(void)
+{
+	if (test_0()) return 1;
+	if (test_1()) return 1;
 	return 0;
 }
